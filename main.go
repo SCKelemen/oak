72aba1ebc756d1d32bffc2ece13acc21f63ea1b2
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2026-09-06 10:56:04 +0300

    Route CLI through fluent compilation pipeline

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/user"
 	"strings"
 
-	"github.com/SCKelemen/oak/codegen"
-	"github.com/SCKelemen/oak/object"
-	"github.com/SCKelemen/oak/parser"
+	"github.com/SCKelemen/oak/compiler"
 	"github.com/SCKelemen/oak/repl"
-	"github.com/SCKelemen/oak/scanner"
-	"github.com/SCKelemen/oak/typechecker"
 )
 
 func main() {
@@ -24,54 +19,24 @@ func main() {
 			os.Exit(1)
 		}
 
-		// Read source file
-		source, err := ioutil.ReadFile(filename)
+		source, err := os.ReadFile(filename)
 		if err != nil {
 			fmt.Printf("Error reading file: %v\n", err)
 			os.Exit(1)
 		}
 
-		// Parse
-		l := scanner.New(string(source))
-		p := parser.New(l)
-		program := p.ParseProgram()
-
-		if len(p.Errors()) > 0 {
-			fmt.Printf("Parser errors:\n")
-			for _, err := range p.Errors() {
-				fmt.Printf("  %s\n", err)
-			}
-			os.Exit(1)
-		}
-
-		// Type check
-		env := object.NewEnvironment()
-		tc := typechecker.New(env)
-		tc.CheckProgram(program)
-
-		if len(tc.Errors()) > 0 {
-			fmt.Printf("Type errors:\n")
-			for _, err := range tc.Errors() {
-				fmt.Printf("  %s\n", err)
-			}
-			os.Exit(1)
-		}
-
-		// Generate C code
-		cg := codegen.New("main", tc)
-		cg.SetSourceFile(filename)
-		cg.SetSourceText(string(source))
-		output, err := cg.Generate(program, tc)
+		output, err := compiler.New().
+			WithSource(filename, string(source)).
+			EmitC().
+			Get()
 		if err != nil {
-			fmt.Printf("Code generation error: %v\n", err)
+			fmt.Printf("Compilation error: %v\n", err)
 			os.Exit(1)
 		}
 
-		// Write output file
 		baseName := strings.TrimSuffix(filename, ".oak")
 		outputFile := baseName + ".c"
-		err = ioutil.WriteFile(outputFile, []byte(output), 0644)
-		if err != nil {
+		if err := os.WriteFile(outputFile, []byte(output), 0644); err != nil {
 			fmt.Printf("Error writing output file: %v\n", err)
 			os.Exit(1)
 		}
