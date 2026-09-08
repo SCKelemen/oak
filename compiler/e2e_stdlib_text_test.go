2693dcfdf514eee07df86e6e0f52e3cbf980afc2
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2026-09-08 12:14:02 +0200

    test(strings): locate unsupported bootstrap syntax

--- compiler/e2e_stdlib_text_test.go
+++ compiler/e2e_stdlib_text_test.go
@@ -1,6 +1,12 @@
 package compiler
 
-import "testing"
+import (
+ "testing"
+ "strings"
+ "github.com/SCKelemen/oak/ast"
+ "github.com/SCKelemen/oak/stdlib"
+ "github.com/SCKelemen/oak/typechecker"
+)
 
 func TestE2EStdlibTextSmoke(t *testing.T) {
 	code, abnormal := buildAndRun(t, "textsmoke", `
@@ -37,4 +43,16 @@ main: (): i32 {
 	if abnormal || code != 42 {
 		t.Fatalf("exit=(%d,%v)", code, abnormal)
 	}
+}
+
+func TestE2EStdlibTextSyntax(t *testing.T) {
+ tree, err := New().WithSource("std.oak", stdlib.Source).Parse().Get()
+ if err != nil { t.Fatal(err) }
+ for i, stmt := range tree.Root.Statements {
+  tc := typechecker.New()
+  tc.CheckProgram(&ast.Program{Statements: tree.Root.Statements[:i+1]})
+  for _, e := range tc.Errors() {
+   if strings.Contains(e, "nil expression") { t.Fatalf("first nil expression at declaration %d %s", i, declarationName(stmt)) }
+  }
+ }
 }
