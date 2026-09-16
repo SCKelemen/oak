f82d8f5990e4ba1a048fec0f81963961881c1d27
Samuel Kelemen <samuel@kelemen.com>
2026-09-16 19:33:40 +0200

    formal: register new proof modules

--- spec/lean/Oak.lean
+++ spec/lean/Oak.lean
@@ -18,6 +18,7 @@ import Oak.AArch64Memory
 import Oak.AArch64WeakMemory
 import Oak.AArch64Encoding
 import Oak.AArch64ReturnEncoding
+import Oak.AArch64DirectBranchEncoding
 import Oak.AArch64Barrier
 import Oak.AArch64Stage2Maintenance
 import Oak.AArch64Mmio
@@ -54,6 +55,7 @@ import Oak.ClosureCapture
 import Oak.BoundedLoop
 import Oak.BorrowStateRefinement
 import Oak.TypeLatticeRefinement
+import Oak.TypeLatticeAtomIdentity
 import Oak.ADTSemantics
 import Oak.SemanticRecord
 import Oak.Variadic
@@ -207,6 +209,7 @@ import Oak.TseitinCNF
 import Oak.CNFBuilderTrace
 import Oak.CNFFinalObligation
 import Oak.CNFTermRoot
+import Oak.CNFBitwiseWordRoot
 import Oak.NativeEqualityCertificate
 import Oak.RVWMO
 import Oak.Reduction
